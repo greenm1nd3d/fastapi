5acce4372f95f8846dbcbb0871ba494c0b1c79c9
Fred Reyes <j2reyes@yahoo.com>
2022-11-27 20:10:52 +0800

    Fix for AWS Lambda deployment

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -5,11 +5,12 @@ from app.config.db import tasks_collection
 from app.models.tasks import TaskSchema
 from app.schemas.tasks import task_serializer, tasks_serializer
 from app.auth.bearer import jwtBearer
+from app.auth.handler import decodeJWT
 
 task_router = APIRouter()
 
 # Get all tasks
-@task_router.get("/", tags=["Tasks"])
+@task_router.get("/tasks", tags=["Tasks"])
 async def get_tasks():
     tasks = tasks_serializer(tasks_collection.find())
     return {
@@ -18,7 +19,7 @@ async def get_tasks():
     }
 
 # Get specific task with given id
-@task_router.get("/{id}", dependencies=[Depends(jwtBearer())], tags=["Tasks"])
+@task_router.get("/task/{id}", dependencies=[Depends(jwtBearer())], tags=["Tasks"])
 async def get_task(id: str):
     task = tasks_serializer(tasks_collection.find({"_id": ObjectId(id)}))
     return {
@@ -27,7 +28,7 @@ async def get_task(id: str):
     }
 
 # Create a new task
-@task_router.post("/", tags=["Tasks"])
+@task_router.post("/task", tags=["Tasks"])
 async def create_task(task: TaskSchema):
     _id = tasks_collection.insert_one(dict(task))
     task = tasks_serializer(tasks_collection.find({"_id": _id.inserted_id}))
@@ -37,7 +38,7 @@ async def create_task(task: TaskSchema):
     }
 
 # Update a task with given id
-@task_router.put("/{id}", tags=["Tasks"])
+@task_router.put("/task/{id}", tags=["Tasks"])
 async def update_task(id: str, task: TaskSchema):
     tasks_collection.find_one_and_update({"_id": ObjectId(id)}, {
         "$set": dict(task)
@@ -49,7 +50,7 @@ async def update_task(id: str, task: TaskSchema):
     }
 
 # Delete a task with given id
-@task_router.delete("/{id}", tags=["Tasks"])
+@task_router.delete("/task/{id}", tags=["Tasks"])
 async def delete_task(id: str):
     tasks_collection.find_one_and_delete({"_id": ObjectId(id)})
     return {
@@ -58,7 +59,7 @@ async def delete_task(id: str):
     }
 
 # Mark a task with given id as completed
-@task_router.patch("/complete/{id}", tags=["Tasks"])
+@task_router.patch("/task/{id}/complete", tags=["Tasks"])
 async def complete_task(id: str, task: TaskSchema):
     tasks_collection.find_one_and_update({"_id": ObjectId(id)}, {
         "$set": {"status": "C"}
--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from typing import Union
-from fastapi import FastAPI, Body, Depends
-from pydantic import BaseModel
-from pymongo import MongoClient
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
 
 from app.routes.tasks import task_router
 from app.routes.users import user_router
 
 app = FastAPI()
+handler = Mangum(app)
+
+origins = [
+    "https://zkjfxwkzcpldgj2eolfdwytfba0sravc.lambda-url.us-west-2.on.aws",
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Tasks API"}
 
 app.include_router(task_router)
 app.include_router(user_router)
